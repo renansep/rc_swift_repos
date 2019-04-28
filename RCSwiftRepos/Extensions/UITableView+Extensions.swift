a2d54f3a42c0d9fade50326228d6d657e28e6523
Renan Cargnin <renan.cargnin@timeneon.com.br>
2019-04-27 21:12:02 -0300

    Implemented UITableView extension.

---
+++ RCSwiftRepos/Extensions/UITableView+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UITableView+Extensions.swift
+//  RCSwiftRepos
+//
+//  Created by Renan Cargnin on 27/04/19.
+//  Copyright © 2019 Renan Cargnin. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func registerNibForCell(withClass classType: AnyClass) {
+        let className = String(describing: classType)
+        register(
+            UINib(nibName: className, bundle: Bundle(for: classType)),
+            forCellReuseIdentifier: className
+        )
+    }
+    
+    func dequeueReusableCell<T>(withClass classType: T.Type) -> T {
+        return dequeueReusableCell(withIdentifier: String(describing: classType)) as! T
+    }
+}
